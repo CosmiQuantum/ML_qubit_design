0b84b1ab806b58f82c1246c2520e98acab006b2c
OliviaSeidel <seidelolivia2@gmail.com>
2024-11-22 19:42:36 +0000

    Restore scripts for other two models that work properly

--- coupler_NCap_cap_matrix/parameters.py
+++ coupler_NCap_cap_matrix/parameters.py
@@ -3,9 +3,10 @@ DATA_DIR = 'data'
 DATASETS_JSON = DATA_DIR + '/datasets.json'
 
 KERAS_TUNER = False
-KERAS_TUNER_TRIALS = 2
+KERAS_TUNER_TRIALS = 30
+KERAS_DIR = 'keras'
 
-ENCODING_TYPE = 'Try Both' # need to pass 'One Hot' or 'Linear' or 'Try Both'
+ENCODING_TYPE = 'One Hot' # need to pass 'One Hot' or 'Linear' or 'Try Both'
 
 # Enable data augmentation/scaling, etc
 DATA_AUGMENTATION = True
@@ -14,14 +15,14 @@ LOG_DRAIN_CURRENT = True
 # We use a simple fully connected network (MLP) 
 # 4 layers because deeper NNs can capture more complex patterns
 # Gradually decrease the neuron size to better capture patterns while avoiding overfitting
-NEURONS_PER_LAYER = [3300, 200, 4600, 1700]
+NEURONS_PER_LAYER = [1600,1300,700,600]
 TRAIN_DROPOUT_RATE = 0.4
 
 # Training hyper-parameters
 
 # Learning Rate gives the step size that the optimizer takes while learning, 
 # smaller step size means slower convergence but more accuracy
-LR_INITIAL = 0.0014558 #0.00020597
+LR_INITIAL = 0.001
 
 # Learning rate decay helps the model become refined as it gets closer to a minimum
 # The learning rate decay steps desides how many steps the learning rate will decay after
